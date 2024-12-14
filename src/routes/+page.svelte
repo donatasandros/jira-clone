fbc57cc988a4b76317d748126eb96f341a527eee
donatas <donatas.andros@gmail.com>
2024-12-14 18:30:58 +0200

    update some components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,12 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div>hello world</div>
-<Button>Click me</Button>
+<div class="space-x-2 p-4">
+	<Button variant="primary">Click me</Button>
+	<Button variant="secondary">Click me</Button>
+	<Button variant="destructive">Click me</Button>
+	<Button variant="ghost">Click me</Button>
+	<Button variant="muted">Click me</Button>
+	<Button variant="outline">Click me</Button>
+	<Button variant="teritary">Click me</Button>
+</div>
